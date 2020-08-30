162253980529382d1821d2163fc042866185f883
Filip Mularski <mularskif@gmail.com>
2020-08-30 23:08:17 +0200

    grid

---
+++ grid.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+
+root = Tk()
+
+label1 = Label(root, text='Hello World')
+label2 = Label(root, text='Bye World')
+
+label1.grid(row=0, column=0)
+label2.grid(row=1, column=0)
+
+root.mainloop()
