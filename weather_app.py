ac339984a91b01e50245ed5a59204288ec240827
Filip Mularski <mularskif@gmail.com>
2020-09-01 20:40:04 +0200

    weather app: entry for desired zipcode and displaying the result

--- weather_app.py
+++ weather_app.py
@@ -3,32 +3,48 @@ import requests
 import json
 from api_keys import weather_app_key
 
+
 root = Tk()
 root.title('Weather app')
-root.geometry('600x50')
+root.geometry('600x100')
 
-city = ''
-quality = 0
-category = ''
+zip_entry = Entry(root)
+zip_entry.grid(row=0, column=0, padx=(5, 0))
 
-category_color = {'Good': '#00e400', 'Moderate': '#ffff00', 'Unhealthy for Sensitive Groups': '#ff7e00',
-                  'Unhealthy': '#ff0000', 'Very Unhealthy': '#8f3f97', 'Hazardous': '#7e0023'}
+result_label = Label(root, font=('Helvetica', 15))
+result_label.grid(row=1, column=0, columnspan=2, padx=(5, 0))
 
-try:
-    api_request = requests.get('http://www.airnowapi.org/aq/observation/zipCode/current/'
-                               '?format=application/json&zipCode=20002&distance=25&API_KEY=' + weather_app_key)
-    api = json.loads(api_request.content)
-    city = api[0]['ReportingArea']
-    quality = api[0]['AQI']
-    category = api[0]['Category']['Name']
 
-    root.configure(background=category_color[category])
-except Exception as e:
-    api = 'Error'
+def zip_lookup():
+    category_color = {'Good': '#00e400', 'Moderate': '#ffff00', 'Unhealthy for Sensitive Groups': '#ff7e00',
+                      'Unhealthy': '#ff0000', 'Very Unhealthy': '#8f3f97', 'Hazardous': '#7e0023',
+                      'Error': '#ffffff'}
+    zip_code = zip_entry.get()
 
-label = Label(root, text=city + ' Air Quality: ' + str(quality) + ' ' + category, font=('Helvetica', 20),
-              background=category_color[category])
-label.pack()
+    try:
+        api_request = requests.get(f'http://www.airnowapi.org/aq/observation/zipCode/current/'
+                                   f'?format=application/json&zipCode={zip_code}&distance=25&API_KEY={weather_app_key}')
+        api = json.loads(api_request.content)
+        city = api[0]['ReportingArea']
+        quality = api[0]['AQI']
+        category = api[0]['Category']['Name']
+
+        # set label text
+        result_label['text'] = city + ' Air Quality: ' + str(quality) + ' ' + category
+
+        # set label and root bg color
+        result_label['background'] = category_color[category]
+        root.configure(background=category_color[category])
+    except Exception as e:
+        result_label['text'] = 'Sorry, an error has occurred.'
+        result_label['background'] = category_color['Error']
+        root.configure(background=category_color['Error'])
+    finally:
+        zip_entry.delete(0, END)
+
+
+zip_button = Button(root, text='Lookup zipcode', command=zip_lookup)
+zip_button.grid(row=0, column=1, padx=(5, 0), pady=(5, 0))
 
 
 root.mainloop()
